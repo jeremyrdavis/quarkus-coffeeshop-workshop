3d58dc5be6fd9f1d2a2b8f79d83aefcb04f61ba0
Jeremy Davis <jeremyrdavis@mac.com>
2020-09-17 23:18:52 -0400

    completed step 1

--- src/test/java/org/j4k/workshops/quarkus/ExampleResourceTest.java
+++ src/test/java/org/j4k/workshops/quarkus/ExampleResourceTest.java
@@ -6,8 +6,14 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+
 @QuarkusTest
 public class ExampleResourceTest {
+
+    @ConfigProperty(name="hello.message")
+    String helloMessage;
 
     @Test
     public void testHelloEndpoint() {
@@ -15,7 +21,7 @@ public class ExampleResourceTest {
           .when().get("/hello")
           .then()
              .statusCode(200)
-             .body(is("hello"));
+             .body(is(helloMessage));
     }
 
 }
